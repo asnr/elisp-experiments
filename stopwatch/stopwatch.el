4642f247afccd3c0563e28a49df29b2018aa74cf
asnr <asnr@users.noreply.github.com>
2017-09-28 22:37:34 +1000

    Cancel timer when buffer is killed

--- stopwatch/stopwatch.el
+++ stopwatch/stopwatch.el
@@ -1,15 +1,25 @@
-(defvar stopwatch-elapsed-seconds 0)
+;; -*- lexical-binding: t; -*-
 
 (defun update-stopwatch (stopwatch-buffer)
   (with-current-buffer stopwatch-buffer
-    (erase-buffer)
-    (setq stopwatch-elapsed-seconds (1+ stopwatch-elapsed-seconds))
-    (insert (number-to-string stopwatch-elapsed-seconds))))
+    (insert (number-to-string 1))))
+
+(defun buffers= (buffer other-buffer)
+  (if (string= (buffer-name buffer) (buffer-name other-buffer))
+      t
+    nil))
 
 (progn
   (setq stopwatch-buffer (generate-new-buffer "stopwatch"))
   (with-current-buffer stopwatch-buffer
-    (insert (number-to-string stopwatch-elapsed-seconds)))
+    (insert (number-to-string 0)))
   (setq new-window (split-window))
   (set-window-buffer new-window stopwatch-buffer)
-  (run-at-time "3 sec" nil 'update-stopwatch stopwatch-buffer))
+  (let ((stopwatch-timer
+         (run-at-time t 1 'update-stopwatch stopwatch-buffer)))
+    (add-hook 'kill-buffer-hook
+              #'(lambda () (when (buffers= (current-buffer) stopwatch-buffer)
+                             (print (concat "Stop timer at buffer " (buffer-name)))
+                             (cancel-timer stopwatch-timer))))))
+
+
